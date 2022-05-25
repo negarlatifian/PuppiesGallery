5aaccceff4b4c61c695ec2f4fdbf224b1f1367ba
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2022-05-25 20:48:08 +0100

    unsplash added

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { PuppyType } from "../../types";
 import UpdateForm from "../UpdateForm/UpdateForm";
 import "./Card.css";
@@ -10,6 +10,7 @@ interface CardPropsComponentType {
 const Card = ({ puppy }: CardPropsComponentType) => {
   const [click, setClick] = useState(false);
   const [info, setInfo] = useState(false);
+  const [url, setUrl] = useState('');
 
   const updateHandler = () => {
     setClick(!click);
@@ -27,11 +28,29 @@ const Card = ({ puppy }: CardPropsComponentType) => {
     fetch(`/api/puppies/${puppy.id}`, requestOptions);
     window.location.reload();
   };
+  const fetchUnsplash = async () => {
+    const data = await fetch(
+      `https://api.unsplash.com/search/photos?page=1&query=${puppy.breed}&client_id=wiSYEYCauBvmzu_WWKR3a6cSkmt3OKdxOg4AQo8oEaM`
+    );
+    const jsonData = await data.json();
+    console.log(jsonData)
+    if(jsonData.results.length === 0){
+      setUrl('https://www.rit.edu/nsfadvance/sites/rit.edu.nsfadvance/files/default_images/photo-unavailable.png')
+    } else {
+      let i = Math.floor(Math.random()*10);
+      const result = jsonData.results[i];
+      setUrl(result.urls.small)
+    }
+  }
+  useEffect(() => {
+    fetchUnsplash();
+  }, [])
 
   return (
     <div className="container">
       <div onClick={infoHandler} className="container__header">
-        <img src={puppy.url} alt="puppy img" />
+        {/* <img src={puppy.url} alt="puppy img" /> */}
+         <img src= {url} alt="puppy img"/>
         <h1>name: {puppy.name}</h1>
       </div>
       {info && (
